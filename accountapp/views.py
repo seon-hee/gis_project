972138ca55492f45ffce09c6925212fcaa4388b6
seon-hee <loooooky1234@gmail.com>
2021-07-08 18:20:47 +0900

    7/8 practice 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,15 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input')
+
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'POST METHOD'})
+                      context={'new_data':new_data})
     else:
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD'})
+                      context={'text':'GET METHOD!'})
