ddacf795121691e9caeed7be5d763894b86015d9
seon-hee <loooooky1234@gmail.com>
2021-07-13 13:10:41 +0900

    7/12 practice 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
 
+from accountapp.models import HelloWorld
 
 def hello_world(request):
     if request.method == "POST":
@@ -13,10 +15,8 @@ def hello_world(request):
         new_data.text = temp
         new_data.save()
 
-        data_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
     else:
         data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
