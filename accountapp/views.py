b78d16f869719fed830f6835038c5cfdd13acc63
seon-hee <loooooky1234@gmail.com>
2021-07-19 18:17:36 +0900

    7/19 practice 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
 def hello_world(request):
@@ -39,13 +40,10 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
-    form_class = UserCreationForm
+    form_class = AccountCreationForm
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
-
-
-
 
 
 class AccountDeleteView(DeleteView):
@@ -53,3 +51,4 @@ class AccountDeleteView(DeleteView):
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
+
