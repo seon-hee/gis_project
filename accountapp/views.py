4e68d835fa18589995ac0f990994c3af91ad5ff7
seon-hee <loooooky1234@gmail.com>
2021-07-22 17:03:16 +0900

    7/22 commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,34 +1,30 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
-@login_required(login_url=reverse_lazy('accountapp:login'))
+
+@login_required
 def hello_world(request):
     if request.method == "POST":
-
         temp = request.POST.get('input')
-
         new_data = HelloWorld()
         new_data.text = temp
         new_data.save()
-
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
     else:
         data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
-
 
 class AccountCreateView(CreateView):
     model = User
@@ -41,8 +37,12 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+
+has_ownership = [login_required, account_ownership_required]
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -51,8 +51,8 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
