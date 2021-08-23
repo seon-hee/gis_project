9c84249d7dbf6b2a0c07030607074a06009a8b5f
seon-hee <loooooky1234@gmail.com>
2021-08-23 18:19:23 +0900

    8/23 commit 5

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import RedirectView
+
+from articleapp.models import Article
+from likeapp.models import LikeRecord
+
+
+class LikeArticleView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_pk'])
+
+        likeRecord = LikeRecord.objects.filter(user=user,
+                                               article=article)
+        if likeRecord.exists():
+            pass
+        else:
+            pass
+
+        return super().get(request, *args, **kwargs)
